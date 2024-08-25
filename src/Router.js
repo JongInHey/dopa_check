91eac6557f01fe413d9949226eb5ee6c5b64ef03
JongInHey <youyi1234@naver.com>
2024-08-26 01:19:40 +0900

    header, footer setup-1

--- src/Router.js
+++ src/Router.js
@@ -3,16 +3,20 @@ import { Home } from "./pages/home/Home";
 import { Choice } from "./pages/qna/Choice";
 import { Result } from "./pages/result/Result";
 import { PageNotFound } from "./pages/PageNotFound";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <HashRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/qna" element={<Choice />} />
         <Route path="/result" element={<Result />} />
         <Route path="/*" element={<PageNotFound />} />
       </Routes>
+      <Footer />
     </HashRouter>
   );
 }
